247d265073bd4cf0a352ecb81038850720d523be
zhadjah9559 <zhadjah@gmail.com>
2020-09-21 21:59:16 -0400

    Made the Login Area

---
+++ HelpDeskTicket/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelpDeskTicket.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
