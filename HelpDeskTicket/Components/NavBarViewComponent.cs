0036eae1a8b88b679e2527409b66162be908d1c2
zhadjah9559 <zhadjah@gmail.com>
2020-11-10 03:56:58 -0500

    turned navbar into a view component

---
+++ HelpDeskTicket/Components/NavBarViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace HelpDeskTicket.Views.Shared.Components
+{
+    public class NavBarViewComponent : ViewComponent
+    {
+        public async Task <IViewComponentResult> InvokeAsync ()
+        {
+            return View();
+        }
+    }
+}
